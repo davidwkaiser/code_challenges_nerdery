75af53176ef67cb05954e32def46e74a225300a6
davidwkaiser <davidwkaiser@yahoo.com>
2017-04-17 22:46:16 -0500

    add models and controllers for snacks and locations

---
+++ app/models/location.rb
@@ -0,0 +1,10 @@
+class Location < ApplicationRecord
+  validates :location, presence: true
+  validates :location, length: { maximum: 50,
+        too_long: "%{count} characters is the maximum allowed" }
+  validates :latitude, :longitude, numericality: true
+  validates_inclusion_of :latitude, in -90..90
+  validates_inclusion_of :longitude, in -180..180
+
+  has_many :snacks
+end
