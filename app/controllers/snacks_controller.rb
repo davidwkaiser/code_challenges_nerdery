db9c978e4b71a274de2efe156fc1e911e108104a
davidwkaiser <davidwkaiser@yahoo.com>
2017-04-18 23:40:13 -0500

    create successful API test

--- app/controllers/snacks_controller.rb
+++ app/controllers/snacks_controller.rb
@@ -1,5 +1,11 @@
+require 'json'
 class SnacksController < ApplicationController
   def index
+    response = HTTParty.get('https://api-snacks.nerderylabs.com/v1/snacks/?ApiKey=bd634d7d-d5c9-427c-bc09-00767e231f31')
+    response.each do |item|
+      Snack.create(name: item["name"])
+    end
+    @snacks = Snack.all
 
   end
 
--- app/models/snack.rb
+++ app/models/snack.rb
@@ -1,8 +1,19 @@
 class Snack < ApplicationRecord
+  def name
+    @name
+  end
+
+  def name=(value)
+    @name=value
+  end
+
   # validates :name, :location, presence: true #confirm if location validated here
   # validates :name, length: { maximum: 200,
   #       too_long: "%{count} characters is the maximum allowed" }
 
   # has_many :locations
   has_many :votes
+
+
+
 end
